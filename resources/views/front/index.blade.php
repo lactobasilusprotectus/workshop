8e9f3aca39c4282d5c59a45f574887095ae233b9
lactobasilusprotectus <amru1731218@sttpln.ac.id>
2020-12-02 22:32:11 +0700

    wip

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -154,7 +154,7 @@
 
     <hr class="mx-auto border-blue-500 my-5 w-1/4">
 
-    <div class="bg-gray-200 dark:bg-gray-700 py-5 mt-5">
+    <div class="layout bg-gray-200 dark:bg-gray-700 py-5 mt-5">
         <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
             <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                 <span class="block">Ready to dive in?</span>
